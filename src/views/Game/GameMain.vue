a73d842adaa12ab81a9ccf9a3a1952897c55a38e
SmallTeddy <zhangzeming1212@gmail.com>
2024-02-23 02:03:35 +0800

    fix: sky and earth show

--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -14,15 +14,11 @@ const timeZhi = lunar.getTimeZhi();
 
 <template>
   <div h-full w-screen color-white p8 flex="~ col" gap-8>
-    <div h-full w-full of-hidden flex-center border="1px dashed #666" border-rd-4  p8 bg-mask flex-col>
-      <div font-size-32px>年干支：{{ yearGan }}</div>
-      <div font-size-32px>年地支：{{ yearZhi }}</div>
-      <div font-size-32px>月干支：{{ monthGan }}</div>
-      <div font-size-32px>月地支：{{ monthZhi }}</div>
-      <div font-size-32px>日干支：{{ dayGan }}</div>
-      <div font-size-32px>日地支：{{ dayZhi }}</div>
-      <div font-size-32px>时干支：{{ timeGan }}</div>
-      <div font-size-32px>时地支：{{ timeZhi }}</div>
+    <div h-full w-full of-hidden flex-center flex-col font-size-32px>
+      <div>年干支：{{ yearGan }}{{ yearZhi }}</div>
+      <div>月干支：{{ monthGan }}{{ monthZhi }}</div>
+      <div>日干支：{{ dayGan }}{{ dayZhi }}</div>
+      <div>时干支：{{ timeGan }}{{ timeZhi }}</div>
     </div>
   </div>
 </template>
