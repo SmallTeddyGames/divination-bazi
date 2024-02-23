8c87cff77d4282d9991bc1c6fedd9d4c253b0899
SmallTeddy <zhangzeming1212@gmail.com>
2024-02-23 13:44:58 +0800

    feat: add component

---
+++ src/views/Game/StepCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div h-340px w-600px of-hidden flex-center font-size-32px flex-col gap-4 border="1px dashed #fff" rounded-4 bg-mask>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts' setup>
+    
+</script>
+
+<style scoped lang='scss'>
+</style>
---
+++ src/views/Game/ThreeStep.vue
@@ -0,0 +1,10 @@
+<template>
+  <StepCard></StepCard>
+</template>
+
+<script lang='ts' setup>
+import StepCard from './StepCard.vue';
+</script>
+
+<style scoped lang='scss'>
+</style>
